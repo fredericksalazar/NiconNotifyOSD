2f0a549692bce7c6a068f796df9070e48799462b
NiconDevTeam <fredefass01@gmail.com>
2014-11-09 00:21:25 -0500

    Se elimina por completo las windowNotify se modifica NiconNotifyOSD para su version 1.0 se prepara el Api Notification para la version 1.0 se mejora documentacion del proyecto

--- src/nicon/notify/core/util/UbuntuFont.java
+++ src/nicon/notify/core/util/UbuntuFont.java
@@ -21,7 +21,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package nicon.notify.core.font;
+package nicon.notify.core.util;
 
 import java.awt.Font;
 import java.awt.FontFormatException;
