281e969c4980603047052b6472e52c9043976270
Frederick Adolfo Salazar Sanchez <fredefass01@gmail.com>
2015-08-22 15:27:09 -0500

    se crea el serverThread que se encarga de la gestion de cierre de las notificaciones

---
+++ src/nicon/notify/core/server/ServerThread.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nicon.notify.core.server;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author TechJunior
+ */
+public class ServerThread extends Thread{
+    
+    private final ServerOSD server;
+    private final int nid;
+    private final int time;
+    
+    public ServerThread(ServerOSD server,int nid, int time){
+        this.server = server;
+        this.nid = nid;
+        this.time = time;
+    }
+    
+    @Override
+    public void run(){
+        try {
+            System.out.println("Server OSD: init run ServerThread process ...");
+            sleep(time);
+            server.remove(nid);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
