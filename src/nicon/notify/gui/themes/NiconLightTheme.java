6795422c244093483c3210ef8f69193a35751f63
Frederick Adolfo Salazar Sanchez <fredefass01@gmail.com>
2015-08-27 13:48:54 -0500

    Se pule el NiconLightTheme haciendolo mas fresco moderno y sobre todo efectivo

--- src/nicon/notify/gui/themes/NiconLightTheme.java
+++ src/nicon/notify/gui/themes/NiconLightTheme.java
@@ -1,13 +1,37 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) NiconSystemCO 2015
+ * License: GPLv3
+ *
+ * Authors:
+ * Frederick Adolfo Salazar Sanchez <fredefass01@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as
+ *   published by the Free Software Foundation; either version 3,
+ *   or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 package nicon.notify.gui.themes;
 
 /**
- *
- * @author frederick
+ * NiconLightTheme es una de las bases graficas y de configuracion de UI de las
+ * notificaciones de escritorio NiconNotifyOSD, el NiconLightTheme representa 
+ * un tema de colores claros basados en un fondo de color Blanco y el gris 
+ * oscuro y claro para resaltar el contenido de los textos.
+ * 
+ * @author Frederick Adolfo Salazar Sanchez
  */
+
 public class NiconLightTheme extends NiconTheme{
     
     private final String bakcgroundPanel;
@@ -19,17 +43,23 @@ public class NiconLightTheme extends NiconTheme{
     
     private static NiconLightTheme instance;
     
+    
+    /**
+     * Crea un objeto de tipo NiconTheme adoptando las caracteristicas de 
+     * NiconLightTheme con un fondo claro y fuentes grises.
+     */
+    
     private NiconLightTheme(){
-        this.bakcgroundPanel = "ffffff";
-        this.titleForeground = "3d3d3d";
-        this.messageForeground = "3d3d3d";
-        this.titleWarningForeground = "f39c12";
-        this.titleErrorForeground = "e74c3c";
-        this.titleOKForeground = "3498db";
+        this.bakcgroundPanel = "F8F8F8";
+        this.titleForeground = "313030";
+        this.messageForeground = "777777";
+        this.titleWarningForeground = "C34000";
+        this.titleErrorForeground = "C91313";
+        this.titleOKForeground = "006B98";
     }
 
     /**
-     *
+     * Retorna el color de fondo de la notificacion
      * @return
      */
     @Override
@@ -37,8 +67,9 @@ public class NiconLightTheme extends NiconTheme{
         return bakcgroundPanel;
     }
     
+    
     /**
-     *
+     * Retorna el color de fuente del titulo de la notificacion
      * @return
      */
     @Override
@@ -46,8 +77,9 @@ public class NiconLightTheme extends NiconTheme{
         return titleForeground;
     }
 
+    
     /**
-     *
+     * Retorna el color de fuente del mensaje de la notificacion
      * @return
      */
     @Override
@@ -56,7 +88,7 @@ public class NiconLightTheme extends NiconTheme{
     }
 
     /**
-     *
+     * Retorna el color para eventos de advertencia del titulo de la notificacion
      * @return
      */
     @Override
@@ -65,7 +97,7 @@ public class NiconLightTheme extends NiconTheme{
     }
     
     /**
-     *
+     * Retorna el color de Error del titulo de la notificacion
      * @return
      */
     @Override
@@ -74,7 +106,7 @@ public class NiconLightTheme extends NiconTheme{
     }
     
     /**
-     *
+     * Retorna el color de Exito del titulo de la notificacion
      * @return
      */
     @Override
@@ -83,7 +115,7 @@ public class NiconLightTheme extends NiconTheme{
     }
     
     /**
-     *
+     * Obtiene una instacia del theme para ser aplicada en una Notificacion.
      * @return
      */
     public static NiconLightTheme getInstance(){
