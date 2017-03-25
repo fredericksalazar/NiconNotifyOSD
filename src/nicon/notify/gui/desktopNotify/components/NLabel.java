4521adfb1677515211317e0ea1ca840f005b9b2f
Frederick Adolfo Salazar Sanchez <fredefass01@gmail.com>
2017-03-24 22:55:25 -0500

    Esta clase es reubicada al paquete gui.desktopNotify.components que contiene los componentes creados para la libreria

--- src/nicon/notify/gui/desktopNotify/components/NLabel.java
+++ src/nicon/notify/gui/desktopNotify/components/NLabel.java
@@ -21,7 +21,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package nicon.notify.core.util;
+package nicon.notify.gui.desktopNotify.components;
 
 import javax.swing.JTextArea;
 
